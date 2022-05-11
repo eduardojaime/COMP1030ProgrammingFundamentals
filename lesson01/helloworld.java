7f76a3acaba58ec929370452ea1cab0cd9bd8848
Eduardo Jaime <eduardojaime86@gmail.com>
2022-05-11 17:22:26 -0400

    Create helloworld.java

---
+++ lesson01/helloworld.java
@@ -0,0 +1,11 @@
+// Everything in Java is an Object (which is instantiated based on a Class)
+// This class represents your program
+class HelloWorld {
+    // All objects have behavior (methods/functions) and attritubes
+    // all methods return something, even if it's just 'nothing'
+    // Main method is called by default when the program executed starts
+    public static void main(String[] args) {
+        // Java has serveral built-in libraries to help us getting started
+        System.out.println("Hello World");
+    }
+}
