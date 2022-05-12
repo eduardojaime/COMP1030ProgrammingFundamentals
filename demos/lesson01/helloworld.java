e403d22a8babd8f3f4b3c95816a9bc21772c1ccf
Eduardo Jaime <eduardojaime86@gmail.com>
2022-05-12 17:29:59 -0400

    Create helloworld.java

---
+++ demos/lesson01/helloworld.java
@@ -0,0 +1,11 @@
+// Everything in Java is an object (which are instantiated based on a class)
+// This class represents our program
+class HelloWorld {
+    // Objects have data (attributes) and behavior (methods)
+    // In this case this is a program, so it needs a 'Main' behavior
+    // All methods return something, in this case VOID which means nothing
+    public static void main(String[] args) {
+        // JDK has built-in libraries to help us getting started
+        System.out.println("Hello Summer Semester!");
+    }
+}
