65d758e2f2f8210dec8e10871f0c0787e567a3a7
Eduardo Jaime <eduardojaime86@gmail.com>
2022-05-18 18:24:40 -0400

    L02 files and examples

---
+++ demos/lab1example/RockPaperScissors.java
@@ -0,0 +1,19 @@
+class RockPaperScissors {
+    // every program has a main method that accepts a list of string arguments
+    public static void main(String[] args) {
+        // code goes here
+        // INPUT
+        // scanner object and get input from user
+        // Math.random() to generate computer input
+
+        // PROCESS
+        // series of if-else statements to determine the results of the game
+        // print out user and computer selection
+        System.out.println("User selected Rock");
+        System.out.println("Computer selected Paper");
+
+
+        // OUTPUT
+        System.out.println("Computer wins!");
+    }
+}
---
+++ lesson02/Welcome.java
@@ -0,0 +1,41 @@
+// Text-printing program.
+
+// In general programs are made of instructions which are made of expressions which are made of operators and operands
+// Hierarchy: Programs > Instructions > Expressions > Operators/Operands
+// Run from Visual Studio by pressing F5
+// Run from Terminal by running the following commands
+//    > javac Welcome
+//    > java Welcome
+public class Welcome {
+    // main method begins execution of Java application
+    public static void main(String[] args) { // curly brackets are used to indicate where a class/method/block starts and ends
+       System.out.println("Welcome to Java Programming!");
+
+       // Multiple instructions can be added
+       // System.out.println("Java Programming!");
+
+       // System is a built-in library
+       // println can print other white-space characters
+       // System.out.println("Welcome\nto\nJava\nProgramming!");
+
+       // println can print out a formatted string using a format expression
+       // System.out.printf("%s%n%s%n", "Welcome to", "Java Programming!");
+
+    } // end method main
+ } // end class Welcome1
+ 
+ 
+ /**************************************************************************
+  * (C) Copyright 1992-2018 by Deitel & Associates, Inc. and               *
+  * Pearson Education, Inc. All Rights Reserved.                           *
+  *                                                                        *
+  * DISCLAIMER: The authors and publisher of this book have used their     *
+  * best efforts in preparing the book. These efforts include the          *
+  * development, research, and testing of the theories and programs        *
+  * to determine their effectiveness. The authors and publisher make       *
+  * no warranty of any kind, expressed or implied, with regard to these    *
+  * programs or to the documentation contained in these books. The authors *
+  * and publisher shall not be liable in any event for incidental or       *
+  * consequential damages in connection with, or arising out of, the       *
+  * furnishing, performance, or use of these programs.                     *
+  *************************************************************************/
