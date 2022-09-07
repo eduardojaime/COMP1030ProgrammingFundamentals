d29a393872f79cf385a3f5df9a21560e592fc92f
Eduardo Jaime <eduardojaime86@gmail.com>
2022-09-07 09:04:41 -0400

    Reorganized repo

---
+++ demos/lesson01a/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace lesson01;
+// class that represents my program
+// everything in C# is an object, even the program itself
+class Program
+{
+    // starting point of execution of this app
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Bonjour mon ami!");
+    }
+}
