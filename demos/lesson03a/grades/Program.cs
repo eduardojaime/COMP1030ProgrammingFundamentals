24ea3e5f7ee3c413d9c5669a2fe4d7e83749c4a3
Eduardo Jaime <eduardojaime86@gmail.com>
2022-09-20 20:49:03 -0400

    l03 part 2

--- demos/lesson03a/grades/Program.cs
+++ demos/lesson03a/grades/Program.cs
@@ -6,27 +6,27 @@ class Program
         Console.WriteLine("Hello Grades!");
         // INPUT 
         Console.WriteLine("Enter a letter: A, B, C, D, E or F");
-        char grade = Console.ReadKey().KeyChar; // readkey listens for a single character input
+        char grade = Convert.ToChar(Console.ReadKey().KeyChar.ToString().ToUpper()); // readkey listens for a single character input
         string result = ""; // variable to store the message
+        Console.WriteLine(); // enters new line
         // PROCESS
         // determine which message to show
-        switch (grade)
-        {
-            case 'A':
-                result = "Excellent!";
+        switch(grade) {
+            case 'A': // A is different than a
+                result ="Excellent!";
                 break; // always break so that other cases don't execute, exits switch statement
-            case 'B':
+            case 'B': // B is different than b
             case 'C':
                 result = "Well done!";
                 break;
             case 'D':
-                result = "You passed...";
+                result ="You passed...";
                 break;
             case 'F':
                 result = "Try again";
                 break;
             default: // if nothing matches
-                result = "Invalid input!";
+                result ="Invalid input!";
                 break;
         }
         // OUTPUT
