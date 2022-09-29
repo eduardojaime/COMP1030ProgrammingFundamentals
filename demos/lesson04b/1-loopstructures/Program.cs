696629526de03498224b7793fd69192364fe3880
Eduardo Jaime <eduardojaime86@gmail.com>
2022-09-29 19:44:52 -0400

    l04 part 1

---
+++ demos/lesson04b/1-loopstructures/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _1_loopstructures;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Loops!");
+        // Setup for while loops
+        Random myRandom = new Random(); // declare and initialize a random object
+        int counter1 = myRandom.Next(1, 11); // random number from 1 to 10 
+        int counter2 = myRandom.Next(1, 11);
+        // while to print an unknown number of ints
+        while (counter1 < 10)
+        {
+            Console.Write($"{counter1.ToString()} "); // 1 2 3 4 5 ...
+            counter1++; // shorthand increment operator, increases value by 1
+        }
+        Console.WriteLine(); // print empty line to move the cursor ahead
+        // do-while to print an unknown number of ints
+        do
+        {
+            Console.Write($"{counter2.ToString()} ");
+            counter2++;
+        }
+        while (counter2 < 5);
+        Console.WriteLine(); // print empty line to move the cursor ahead
+        // for to count to 10
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write($"{i.ToString()} ");
+        }
+        Console.WriteLine();
+        // foreach to print all elements in a list
+        string[] daysOfWeek = { "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY" };
+        foreach (string day in daysOfWeek)
+        {
+            Console.Write($"{day} ");
+        }
+        Console.WriteLine();
+    }
+}
--- lesson04/loopstructures/Program.cs
+++ lesson04/loopstructures/Program.cs
@@ -6,31 +6,38 @@ class Program
         Console.WriteLine("Hello Loops!");
 
         Random myRandom = new Random();
-        int counter1 = myRandom.Next(1,11);
-        int counter2 = myRandom.Next(1,11);
+        int counter1 = myRandom.Next(1, 11);
+        int counter2 = myRandom.Next(1, 11);
         // while
-        while (counter1 < 10) { // condition is checked then code is executed
-            Console.WriteLine(counter1.ToString());
+        while (counter1 < 10)
+        { // condition is checked then code is executed
+            Console.Write($"{counter1.ToString()} ");
             counter1++;
         }
+        Console.WriteLine();
 
         // do-while
-        do {
-            Console.WriteLine(counter1.ToString());
-            counter1++;
+        do
+        {
+            Console.Write($"{counter2.ToString()} ");
+            counter2++;
         }
-        while (counter1 < 10); // code executes then condition is check
+        while (counter2 < 10); // code executes then condition is check
+        Console.WriteLine();
 
         // for
-        for(int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++)
+        {
             Console.Write($"{i.ToString()} ");
         }
         Console.WriteLine();
 
         // foreach
-        string[] daysOfWeek = {"SUNDAY", "MONDAY", "TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"};
-        foreach(string day in daysOfWeek) {
-            Console.WriteLine(day);
+        string[] daysOfWeek = { "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY" };
+        foreach (string day in daysOfWeek)
+        {
+            Console.Write($"{day} ");
         }
+        Console.WriteLine();
     }
 }
