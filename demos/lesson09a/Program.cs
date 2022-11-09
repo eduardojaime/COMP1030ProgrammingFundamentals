9751c185c1d4c8b60f4f993d550c3368f1464b7c
Eduardo Jaime <eduardojaime86@gmail.com>
2022-11-08 20:09:46 -0500

    main game loop

--- demos/lesson09a/Program.cs
+++ demos/lesson09a/Program.cs
@@ -11,18 +11,31 @@ class Program
         int playerTotal = 0; // score
         int computerTotal = 0;  // score
         // variables that affect inner play
-        string choiceCard =""; // Y or N
+        string choiceCard = ""; // Y or N
         bool anotherCard = true; // set to true by default
         // variables that affect main game
-        string choiceGame =""; // Y or N
+        string choiceGame = ""; // Y or N
         bool anotherGame = true;
 
         // Main game loop > play again?, encloses everything
+        while (anotherGame == true)
+        {
+            // setup the game
+            // inner game loop > another card? increase score
+            // computer play
 
-        // setup the game
-        // inner game loop > another card? increase score
-        // computer play
-
-        // OUTPUT determine winner
+            // OUTPUT determine winner
+            
+            // ask user whether they want to play again
+            Console.WriteLine("Good game! Would you like to play again?");
+            choiceGame = Console.ReadLine().Trim().ToUpper(); 
+            if (choiceGame == "Y") {
+                anotherGame = true;
+            }
+            else {
+                anotherGame = false;
+            }
+            Console.WriteLine();
+        }
     }
 }
