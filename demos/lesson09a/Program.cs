749ef6897f73f0d18ba8a9e6f55e8e7788a1c11d
Eduardo Jaime <eduardojaime86@gmail.com>
2022-11-08 20:06:52 -0500

    added variables

--- demos/lesson09a/Program.cs
+++ demos/lesson09a/Program.cs
@@ -4,6 +4,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        
+        // INPUT related Variables
+        // variables that affect score
+        Random myRandom = new Random();
+        int currentCard = 0; // card
+        int playerTotal = 0; // score
+        int computerTotal = 0;  // score
+        // variables that affect inner play
+        string choiceCard =""; // Y or N
+        bool anotherCard = true; // set to true by default
+        // variables that affect main game
+        string choiceGame =""; // Y or N
+        bool anotherGame = true;
+
+        // Main game loop > play again?, encloses everything
+
+        // setup the game
+        // inner game loop > another card? increase score
+        // computer play
+
+        // OUTPUT determine winner
     }
 }
