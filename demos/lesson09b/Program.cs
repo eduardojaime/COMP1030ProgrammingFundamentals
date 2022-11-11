e3a5e16822c3669954a0319772964918ff010658
Eduardo Jaime <eduardojaime86@gmail.com>
2022-11-10 19:32:51 -0500

    l09 set variables

--- demos/lesson09b/Program.cs
+++ demos/lesson09b/Program.cs
@@ -4,5 +4,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        // Variables that affect score
+        Random myRandom = new Random();
+        int currentCard = 0;
+        int playerTotal = 0;
+        int computerTotal = 0;
+        // variables that affect inner game loop
+        string choiceCard = "";
+        bool anotherCard = true;
+        // variables that affect main game loop
+        string choiceGame = "";
+        bool anotherGame = true;
+
+        // Main Game Loop
+        // reset game
+        // inner game loop > another card y/n
+        // generate computer score
+        // determine winner
+        // ask for another game > y/n
     }
 }
