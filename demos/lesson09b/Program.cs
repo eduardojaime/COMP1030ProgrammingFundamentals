930fc2f9997e33c5cef841ab5af532a978188c48
Eduardo Jaime <eduardojaime86@gmail.com>
2022-11-10 19:41:59 -0500

    l09 inner game loop

--- demos/lesson09b/Program.cs
+++ demos/lesson09b/Program.cs
@@ -25,16 +25,35 @@ class Program
             currentCard = 0;
             anotherCard = true; // because we want to give a card to the user at the beginning of the game
             // inner game loop > another card y/n
-            
+            while (anotherCard == true)
+            {
+                // generate number from 1 to 11
+                // add to the player score
+                // ask if they want another card > y/n
+                Console.WriteLine("Would you like another card? Y/N");
+                choiceCard = Console.ReadLine().Trim().ToUpper(); // validate and clean inputs
+                if (choiceCard == "Y")
+                {
+                    anotherCard = true;
+                }
+                else
+                {
+                    anotherCard = false;
+                }
+                Console.WriteLine(); // prints empty line for formatting
+                Console.Clear();
+            }
             // generate computer score
             // determine winner
             // ask for another game > y/n
-            Console.WriteLine("Good game! Do you want to play again?");
+            Console.WriteLine("Good game! Do you want to play again? Y/N");
             choiceGame = Console.ReadLine().Trim().ToUpper(); // validate and clean inputs
-            if (choiceGame == "Y") {
+            if (choiceGame == "Y")
+            {
                 anotherGame = true;
             }
-            else {
+            else
+            {
                 anotherGame = false;
             }
             Console.WriteLine(); // prints empty line for formatting
