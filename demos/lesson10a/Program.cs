21e759103587a377f7a3b0f6e55c342d82feef47
Eduardo Jaime <eduardojaime86@gmail.com>
2022-11-15 20:52:06 -0500

    L10 part 3

--- demos/lesson10a/Program.cs
+++ demos/lesson10a/Program.cs
@@ -6,7 +6,12 @@ class Program
         Console.WriteLine("Hello, Georgian College!");
 
         // 1) Add 1 student and print average
-
+        Student studentA = new Student(10, "Edward", "James", "Programming", Convert.ToDateTime("1986-01-01"));
+        Student studentB = new Student(11, "Jon", "Snow", "Arts", Convert.ToDateTime("1990-01-01"));
+        studentA.Average = 200;
+        studentB.Average = 78;
+        Console.WriteLine($"{studentA.FirstName}'s grade letter is {studentA.GetLetterGrade()}");
+        Console.WriteLine($"{studentB.FirstName}'s grade letter is {studentB.GetLetterGrade()}");
         // 2) Add a list of students and print averages and grade letters
     }
 }
--- lesson10/Student.cs
+++ lesson10/Student.cs
@@ -1,10 +1,86 @@
-public class Student {
-    public string FirstName {get;set;}
-    public string LastName {get;set;}
-    public string EmailAddress {get;set;}
-    public decimal Average {get;set;}
-    public string FullName {get {
-        return $"{this.FirstName} {this.LastName}";
-    }}
+/* design
+    Student
+    --------
+    studentId: int
+    firstName: string
+    lastName: string
+    birthDate: datetime // property > validation
+    program: string
+    average: decimal
+    grades: decimal[]
+    ---------
+    void registerToProgram(program)
+    void enrollToClass(classId)
+    void dropFromClass(classId)
+*/
+public class Student
+{
+    // fields
+    public int StudentId; // get;set; is implicit
+    public string FirstName { get; set; } // optionally you can add get;set; to explicitly indicate this is a field
+    public string LastName;
+    public string Program;
+    // properties (I want validation) > more complex field that can have logic
+    private DateTime _BirthDate; // private field names start with underscore
+    public DateTime BirthDate
+    { // 1800-01-01 or 0001-01-01 ??? are these valid?
+        get
+        {
+            // get is called when the value is retrieved for example student.BirthDate
+            return this._BirthDate; // return normally, no validation needed
+        }
+        set
+        {
+            // set is called when the value is assigned for example student.BirthDate = '1986-01-01'
+            // validate then set or throw error
+            if (value < (DateTime.Now.AddYears(-100)))
+            {
+                throw new ArgumentException("That's not a valid value for the birth date!");
+            }
+            else
+            {
+                this._BirthDate = value;
+            }
+        }
+    }
+    private decimal _Average;
+    public decimal Average
+    {
+        get
+        {
+            return this._Average;
+        }
+        set
+        {
+            // averages go from 0 to 100
+            if (value >= 0 && value <= 100)
+            {
+                this._Average = value;
+            }
+            else
+            {
+                this._Average = 0; // invalid value, set to default 0
+            }
+        }
+    }
+    // Constructor
+    public Student(int studentId, string firstName, string lastName, string program, DateTime birthDate)
+    {
+        this.StudentId = studentId;
+        this.FirstName = firstName;
+        this.LastName = lastName; // best practice is to use camelCase for parameter names
+        this.Program = program;
+        this.BirthDate = birthDate; // I want the validation
+    }
+
+    // Methods > return a letter based on the average score
+    public string GetLetterGrade()
+    {
+        if (this.Average >= 90) { return "A"; }
+        else if (this.Average >= 80) { return "B"; }
+        else if (this.Average >= 70) { return "C"; }
+        else if (this.Average >= 60) { return "D"; }
+        else { return "F"; }
+    }
 
 }
