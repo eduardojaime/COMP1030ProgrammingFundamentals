7bfa5ec446af78d2cfafe0728ba13f6a6dd3ba89
Eduardo Jaime <eduardojaime86@gmail.com>
2022-11-23 07:22:14 -0500

    Create Rectangle.cs

---
+++ demos/lesson11/inheritance/Rectangle.cs
@@ -0,0 +1,26 @@
+using System;
+// colon symbol can be read as 'is a' or 'inherits from'
+public class Rectangle : Shape
+{
+    // FIELDS
+    public double Width;
+    public double Height;
+    // PROPERTIES
+    // CONSTRUCTORS
+    // METHODS
+    // optional override PrintShape()
+    // must override abstract methods in base class
+    public override double CalculateArea()
+    {
+        
+        // A = ab
+        Console.WriteLine("Calculating Rectangle Area");
+        return Width * Height;
+    }
+    public override double CalculatePerimeter()
+    {
+        // P = (a+b) * 2
+        Console.WriteLine("Calculating Rectangle Perimeter");
+        return (Width + Height) * 2;
+    }
+}
