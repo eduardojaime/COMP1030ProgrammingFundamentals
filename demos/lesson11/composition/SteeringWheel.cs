a7f512d7cf320ed183d98b2ab110bc587d22781b
Eduardo Jaime <eduardojaime86@gmail.com>
2022-11-23 07:58:50 -0500

    Create SteeringWheel.cs

---
+++ demos/lesson11/composition/SteeringWheel.cs
@@ -0,0 +1,13 @@
+using System;
+public class SteeringWheel
+{
+    public string Color;
+    public void TurnLeft()
+    {
+        Console.WriteLine("Turning Left!");
+    }
+    public void TurnRight()
+    {
+        Console.WriteLine("Turning Right!");
+    }
+}
