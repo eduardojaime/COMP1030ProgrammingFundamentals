22a026e1ee6f20ed717d50475a58fd21be39157c
Eduardo Jaime <eduardojaime86@gmail.com>
2022-11-23 08:03:39 -0500

    composition demonstration

--- demos/lesson11/composition/Program.cs
+++ demos/lesson11/composition/Program.cs
@@ -3,6 +3,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, Cars!");
+
+        // same as in a factory
+        // engine and steering wheel must exist before we can make a car
+        Engine myEngine = new Engine();
+        myEngine.CylinderCount = 4;        
+
+        SteeringWheel mySteeringWheel = new SteeringWheel();
+        mySteeringWheel.Color = "blue";
+
+        Car myCar = new Car("VW", 1994, "GOLF", mySteeringWheel, myEngine);
+        myCar.GoDrive();
     }
 }
