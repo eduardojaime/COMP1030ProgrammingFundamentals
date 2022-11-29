0f52ca68f6456f844a0314ac6deaa3b869304897
Eduardo Jaime <eduardojaime86@gmail.com>
2022-11-29 18:15:43 -0500

    l12 game example

---
+++ demos/lesson12a/GameExample/Game.cs
@@ -0,0 +1,30 @@
+// Part of Rubric > 20 marks
+// Minimum necessary files: 2
+public class Game
+{
+    // FIELDS AND PROPERTIES
+    // CONSTRUCTORS
+    public Game(int playerNbr, string difficulty)
+    {
+        // TODO
+    }
+    // Example methods
+    public void StartGame()
+    {
+        // TODO
+        Console.WriteLine("Starting the game");
+    }
+
+    public void PlayTurn()
+    {
+        // TODO
+        Console.WriteLine("Playing the game");
+    }
+
+    public void DetermineResult()
+    {
+        // TODO
+        Console.WriteLine("Finishing the game");
+    }
+
+}
---
+++ demos/lesson12a/GameExample/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GameExample;
+class Program
+{
+    // Part of the Rubric > 10 marks
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        bool keepPlaying = true;
+
+        // You should call game related methods from here
+        Game myGame = new Game(2, "easy");
+        myGame.StartGame();
+        while (keepPlaying)
+        {
+            myGame.PlayTurn();
+            keepPlaying = false;
+        }
+        myGame.DetermineResult();
+
+    }
+}
