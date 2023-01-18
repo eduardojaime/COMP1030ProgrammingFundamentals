45b202b638821a4d3d28e652f0705d3dd083efbd
Eduardo Jaime <eduardojaime86@gmail.com>
2023-01-17 20:20:49 -0500

    l02 part 2 declaring variables

--- demos/lesson02a/Program.cs
+++ demos/lesson02a/Program.cs
@@ -3,6 +3,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-    }
+        Console.WriteLine("Hello, Data Types!");
+
+        // declare variables > creates the bucket in memory, but it's empty
+        // whole number data types > default value is 0
+        byte myByte;
+        int myInt;
+        long myLong;
+
+        // real number data types > default value is 0.0
+        float myFloat; // ~ 7 digits, suffix F for float
+        double myDouble; // ~15-16 digits (precision), suffix D for Double
+        decimal myDecimal; // 64 digits (extra precision), suffix M for money
+
+        // text data types > default value is null
+        string myString; // a series of characters, text, no limit
+        char myChar; // single character
+
+        // special data types > default are FALSE, or 01-01-01 00:00:00 for date times
+        bool myBoolean;
+        DateTime myBirthday; 
+
+        // assign values
+
+        // print them out to the console
+
+        // review mathematical operations
+
+        // print results
+
+        // convert values
+
+        // utilize string methods
+    }    
 }
