d9f2c8b6c01dec993d94eb521fda18e862d51f14
Eduardo Jaime <eduardojaime86@gmail.com>
2023-01-17 20:42:02 -0500

    l02 part 2 assigning values and printing out to console

--- demos/lesson02a/Program.cs
+++ demos/lesson02a/Program.cs
@@ -25,8 +25,36 @@ class Program
         DateTime myBirthday; 
 
         // assign values
+        // for integers, data type will be detected by compiler
+        // 8 > any number data type work
+        // 1000 > byte or sbyte won't work... the rest will
+        // myByte = 1000; // red squiggly line
+        myByte = 8; // range 0 to 255
+        // myInt = 8; // range -2billion to +2billion, storing 8 wastes a lot of space in memory
+        myInt = 2000012345; // valid range
+        myLong = 7000000000000000000; // 7 + 18 zeros
+
+        // for floating point numbers, always use the suffix
+        myFloat = 10.99F;
+        myDouble = 10.99D; // no need to use suffix because C# assumes all numbers with digits are doubles
+        myDecimal = 10.99M; 
+
+        myString = "A coffee in France is \u20AC 3"; // remember to escape characters
+        // myChar = (char)8364; // 8364 is the € symbol in Unicode table
+        myChar =  'A'; // single quote, single character
+        // Console.WriteLine(myString);
+        // Console.WriteLine(myChar.ToString());
+
+        myBoolean = true;
+        myBirthday = new DateTime(1986, 09, 16, 07, 00, 00);
 
         // print them out to the console
+        Console.WriteLine($"The value of myInt is {myInt.ToString()}");
+        Console.WriteLine($"The value of myFloat is {myFloat.ToString()}");
+        Console.WriteLine($"The value of myString is {myString}"); // no interpolation
+        Console.WriteLine($"The value of myChar is {myChar}");
+        Console.WriteLine($"The value of myBirthday is {myBirthday.ToShortDateString()}");
+
 
         // review mathematical operations
 
