6ddba9fdf7543f85a457c0b25375f91074f67b3b
Eduardo Jaime <eduardojaime86@gmail.com>
2023-01-18 20:46:26 -0500

    l02 part 2 assigning values

--- demos/lesson02b/Program.cs
+++ demos/lesson02b/Program.cs
@@ -6,7 +6,8 @@ class Program
         Console.WriteLine("Hello, Data!");
         // declare variables
         // whole numbers
-        byte myByte;
+        byte myByte; // unsigned = stores only positive numbers
+        sbyte mySignedByte; // signed = stores negative numbers
         int myInt;
         long myLong;
         // real numbers
@@ -19,10 +20,33 @@ class Program
         // special
         bool myBoolean;
         DateTime myBirthday;
-        
+
         // assign values to these variables and print them out
+        // byte stores 0 to 255
+        // store 1000
+        // myByte = 1000; // breaks because it's a larger number than the bucket size
+        myByte = 8;
+        myInt = 2000012345; // 2 billion
+        myLong = 7000000000000000000; // quintillions > 7 + 18 zeros
+
+        // floating point numbers > always use suffix
+        // 10.99 > default double
+        myFloat = 10.99F; // F tells the compiler that 10.99 is a float
+        myDouble = 10.99D; // doesn't need suffix, C# by default assumes it's double
+        myDecimal = 10.99M;  // M is for Money
+
+        // Text and special ones
+        myText = "A coffee in France is \u20AC 3.00";
+        Console.WriteLine(myText);
+        myChar = 'A'; // single quote single character
+        // myChar = (char)8364; // you can cast int to char and get, this is the decimal representation of Euro symbol
+        Console.WriteLine(myChar);
+
+        myBoolean = true;
+        myBirthday = new DateTime(2023, 09, 16, 07, 00, 00);
 
         // perform mathematical operations on numbers
+        
 
         // convert values
 
