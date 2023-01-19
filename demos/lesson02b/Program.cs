778038c466c9c2c865a70c8c8522fab9e16c2e7e
Eduardo Jaime <eduardojaime86@gmail.com>
2023-01-18 20:26:34 -0500

    l02 part 2 declaring variables

--- demos/lesson02b/Program.cs
+++ demos/lesson02b/Program.cs
@@ -3,6 +3,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, Data!");
+        // declare variables
+        // whole numbers
+        byte myByte;
+        int myInt;
+        long myLong;
+        // real numbers
+        float myFloat; // 7 digits
+        double myDouble; // 16 digits
+        decimal myDecimal; // 64 digits for extra precision
+        // text
+        string myText; // text of any size
+        char myChar; // single character
+        // special
+        bool myBoolean;
+        DateTime myBirthday;
+        
+        // assign values to these variables and print them out
+
+        // perform mathematical operations on numbers
+
+        // convert values
+
+        // string methods for string manipulation
     }
 }
