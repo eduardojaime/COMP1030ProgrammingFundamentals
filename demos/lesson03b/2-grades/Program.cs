c677bab87bf8ca737df2bb0a6dddbf9847bb99e4
Eduardo Jaime <eduardojaime86@gmail.com>
2023-01-25 20:29:25 -0500

    l03 part 2 grades

--- demos/lesson03b/2-grades/Program.cs
+++ demos/lesson03b/2-grades/Program.cs
@@ -3,6 +3,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, Grades!");
+        // Switch
+        // INPUT > Letter representing a grade (CHAR)
+        Console.WriteLine("Enter a letter: A, B, C, D, or F"); // always show what the valid range of values is
+        char grade = Console.ReadKey().KeyChar; // returns ConsoleKeyInfo object, access KeyChar to retrieve actual character
+        string result = "";
+        // PROCESS > comparing that value against a list and generating a message accordingly
+        switch (grade) 
+        {
+            // multiple cases
+            case 'A': // single quote for single characters (char data type)
+                result = "Excellent!";
+                break; // important to add break to prevent program from continuing checking cases
+            case 'B': // OR C
+            case 'C':
+                result = "Well done!";
+                break;
+            case 'D':
+                result = "You passed...";
+                break;
+            case 'F':
+                result = "Please try again. :)";
+                break;
+            // default case for any invalid input
+            default:
+                result = "Invalid Input!";
+                break; // best practice, consistency
+        }
+        // OUTPUT > show message in console
+        Console.WriteLine(result);
+
     }
 }
--- lesson03/grades/Program.cs
+++ lesson03/grades/Program.cs
@@ -5,7 +5,7 @@ class Program
     {
         Console.WriteLine("Hello Grades!");
         // INPUT 
-        Console.WriteLine("Enter a letter: A, B, C, D, E or F");
+        Console.WriteLine("Enter a letter: A, B, C, D, or F");
         char grade = Console.ReadKey().KeyChar; // readkey listens for a single character input
         string result = ""; // variable to store the message
         // PROCESS
