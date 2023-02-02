9b14be8861ffeff0c8ee626d410be41f0e44d6b1
Eduardo Jaime <eduardojaime86@gmail.com>
2023-02-01 20:21:35 -0500

    l04 part 2 loop control

--- demos/lesson04b/2-loopcontrol/Program.cs
+++ demos/lesson04b/2-loopcontrol/Program.cs
@@ -3,6 +3,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, Loop Control!");
+        // show only odd numbers
+        // 1 3 5  7 9 .... 99
+        for (int i = 1; i <= 100; i++)
+        {
+            // modulo operator to calculate a remainder of a division by 2
+            // 100 % 2 = 0 >> even
+            // 3 % 2 = 1 >> odd
+            int remainder = i % 2; // returns 1 if the remainder of dividing i by 2 is greater than 0
+            if (remainder == 0) // even number
+            {
+                // break; // don't want to print these
+                continue;
+            }
+            Console.Write($"{i.ToString()} ");
+        }
     }
 }
