78194c1484cd22ac11b514a0d72fed31fecbf1d9
Eduardo Jaime <eduardojaime86@gmail.com>
2023-02-01 20:07:44 -0500

    Update Program.cs

--- lesson04/loopstructures/Program.cs
+++ lesson04/loopstructures/Program.cs
@@ -3,43 +3,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Loops!");
-
-        Random myRandom = new Random();
-        int counter1 = myRandom.Next(1, 11);
-        int counter2 = myRandom.Next(1, 11);
-        // while
-        while (counter1 < 10)
-        { // condition is checked then code is executed
-            Console.Write($"{counter1.ToString()} ");
-            counter1++;
+        Console.WriteLine("Hello, Loops Structures!");
+        // Demo all different loop structures
+        // Input
+        Random myRandom = new Random(); // random number generator
+        int counter1 = myRandom.Next(1, 11); // assigns random number from 1 to 10 (11 not included)
+        int counter2 = myRandom.Next(1, 11); // assigns a different random number from 1 to 10
+        Console.WriteLine($"Counter1: {counter1.ToString()} Counter2: {counter2.ToString()}");
+        // Process + Output (writeline)
+        // while > condition checked and if true then code executed
+        while (counter1 < 11) // so any number from 1 to 10 will return true
+        {
+            Console.Write($"{counter1.ToString()} "); // write() prints in the same line
+            counter1++; // increment operator
         }
-        Console.WriteLine();
-
-        // do-while
-        do
+        Console.WriteLine(); // print a new line <<
+        // Console.Write("\n\n\n"); // << three spaces
+        // do-while > code executed once and then condition checked
+        do 
         {
             Console.Write($"{counter2.ToString()} ");
             counter2++;
         }
-        while (counter2 < 10); // code executes then condition is check
-        Console.WriteLine();
-
-        // for
-        for (int i = 1; i <= 10; i++)
-        {
-            Console.Write($"{i.ToString()} ");
+        while (counter2 < 11);
+        Console.WriteLine(); // empty line
+        // for > similar execution than while
+        // counter is usually declared and used inside for loop
+        int counter3 = myRandom.Next(1,11);
+        for (int i = counter3; i < 11; i++) {
+            Console.Write($"{i.ToString()} "); // counter somehow used inside the code of for loops
         }
         Console.WriteLine();
-
-        // foreach
-        string[] daysOfWeek = { "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY" };
-        // string[] daysOfWeek = { "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY" };
-
-        foreach (string day in daysOfWeek)
+        // foreach > works with lists and iterates automatically, no need to stop execution or increase counters
+        // string[] daysOfWeek = { "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY" };
+        // Advantage no need to modify code if list changes
+        string[] daysOfWeek = { "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY" }; // most of the time lists come from a data store
+        foreach (string day in daysOfWeek) 
         {
             Console.Write($"{day} ");
         }
         Console.WriteLine();
+        
+        // Advanced question > accessing a collection of keyvalue pairs
+        // Dictionary<string, string> myDictionary = new Dictionary<string, string>();
+        // foreach(KeyValuePair<string, string> pair in myDictionary) {}
+        // foreach(string key in myDictionary.Keys) {}
+        // foreach(string val in myDictionary.Values) {}
     }
 }
