11cf80a3585e7130b5367b3cccd1dd6918138055
Eduardo Jaime <eduardojaime86@gmail.com>
2023-02-06 18:41:41 -0500

    l04 demo files

---
+++ demos/lesson05/arraylists/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+
+namespace arraylists;
+class Program
+{
+    // Reference and examples: 
+    // https://www.programiz.com/csharp-programming/arraylist
+    // https://www.c-sharpcorner.com/UploadFile/3d39b4/arraylist-in-C-Sharp/
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Array List!");
+        // arrays are fixed size so they work only when we know max number of elements we will have
+        // if we don't know how many elements we will have we can use Array List
+        ArrayList studentList = new ArrayList();
+
+        bool addAnother = true;
+        while (addAnother)
+        {
+            Console.WriteLine("Please enter a name to add to the list: ");
+            string studentName = Console.ReadLine();
+
+            studentList.Add(studentName);
+
+            Console.WriteLine("Would you like to add another name? Press Y to continue, ANY other key to exit.");
+            char selection = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            if (selection == 'Y' || selection == 'y')
+            {
+                addAnother = true;
+            }
+            else
+            {
+                addAnother = false; // exit
+            }
+        }
+
+        Console.WriteLine($"There are {studentList.Count} students registered.");
+        Console.WriteLine($"Here's the list:");
+        foreach(string student in studentList) {
+            Console.WriteLine(student);
+        }
+    }
+}
---
+++ demos/lesson05/basicarrays/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace basicarrays;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Arrays!");
+
+        // Array 1 - initialize empty, show default values
+        int[] arr1 = new int[10];
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            Console.WriteLine($"Index: {i.ToString()} Value: {arr1[i].ToString()}");
+        }
+        // Array 2 - initialize array with provided list
+        int[] arr2 = { 20, 30, 10, 400, 123, 34, 76, 23, 123, 22 };
+        for (int i = 0; i < arr2.Length; i++)
+        {
+            Console.WriteLine($"Index: {i.ToString()} Value: {arr2[i].ToString()}");
+        }
+
+        // Array 3 - access/modify the values in the array and then print
+        int[] arr3 = new int[10];
+        // Modify
+        for (int i = 0; i < arr3.Length; i++)
+        {
+            // access current element in position i and modify its value
+            arr3[i] = 2 + 2 * i;
+        }
+        // Print new values
+        for (int i = 0; i < arr3.Length; i++)
+        {
+            Console.WriteLine($"Index: {i.ToString()} Value: {arr3[i].ToString()}");
+        }
+        // Array 4 - find the total sum of the elements in the array
+        int total = 0;
+        int[] arr4 = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+        for (int i = 0; i < arr4.Length; i++)
+        {
+            // access current element in position i and add its value to total
+            total = total + arr4[i];
+        }
+        Console.WriteLine($"Total Sum of Elements is {total.ToString()}");
+    }
+}
