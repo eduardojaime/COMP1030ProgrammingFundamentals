fe72bc035cd8a4382173794ec40e40f845c6bb2e
Eduardo Jaime <eduardojaime86@gmail.com>
2023-03-15 19:33:42 -0400

    l09 draft logic

--- demos/lesson09b/Program.cs
+++ demos/lesson09b/Program.cs
@@ -3,6 +3,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, BlackJack!");
+        // INPUTS
+        // variables that affect the scores
+        // variables that affect the inner game loop
+        // variables that affect the main game loop
+
+        // PROCESS
+        // main game loop (round)
+        //  inner game loop (player)
+        //      give user a card and increase score
+        //      ask if user wants another card
+        //  generate computer score
+
+        // OUTPUTS
+        //  determine winner and show a message
+        //  instant win = 21
+        //  instant lose > 21
+        //  tie comp = player
+        //  normal win player > comp
+        //  normal lose comp > player and everything else I didn't cover
+
+        //  ask player if they want to play again
+        //  yes > repeat game
+        //  no > show goodbye message and end
     }
 }
