8b18c42df2502bfa61f647118be8d98f448ff0fb
Eduardo Jaime <eduardojaime86@gmail.com>
2023-03-21 20:53:55 -0400

    l10 instantiating objects

--- demos/lesson10a/Program.cs
+++ demos/lesson10a/Program.cs
@@ -4,5 +4,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, Georgian College!");
+        DateTime birthDateA = new DateTime(1986, 01, 01);
+        // Instantiate student objects
+        Student studentA; // <<< this is variable and it's empty at this point, NULL in memory
+        studentA = new Student(2023010101, "Edward", "James", "Programmer Analyst", birthDateA); // should break with new DateTime()
+        studentA.Average = 5000;
+        Console.WriteLine($"{studentA.FirstName}'s grade letter is {studentA.GetLetterGrade()}");
+
+        // Another 
+        DateTime birthDateB = new DateTime(1990,01,01);
+        Student studentB = new Student(2023010102, "Jon", "Snow", "Arts", birthDateB);
+        studentB.Average = 78;
+        Console.WriteLine($"{studentB.FirstName}'s grade letter is {studentB.GetLetterGrade()}");
     }
 }
