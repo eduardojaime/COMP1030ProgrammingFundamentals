134f72a86fdf6d547dc5a649d39e131eb53afba6
Eduardo Jaime <eduardojaime86@gmail.com>
2023-03-22 20:34:35 -0400

    l10 example 2

--- demos/lesson10b/Program.cs
+++ demos/lesson10b/Program.cs
@@ -9,8 +9,14 @@ class Program
         DateTime invalidDate = new DateTime(); // default value 0001/01/01 00:00:00 
         // we added validation for the birthdate values
         // Student studentA = new Student(230001, "Edward", "James", "Programmer Analyst", invalidDate); // broken
+        
         DateTime birthDateA = new DateTime(1986, 01, 01);
         Student studentA = new Student(230001, "Edward", "James", "Programmer Analyst", birthDateA); // this would work
+        studentA.Average = 200; // invalid avg range
         Console.WriteLine($"{studentA.FirstName}'s grade letter is {studentA.GetLetterGrade()}");
+
+        Student studentB = new Student(230002, "Jon", "Snow", "Arts", new DateTime(1990, 01,01));
+        studentB.Average = 78;
+        Console.WriteLine($"{studentB.FirstName}'s grade letter is {studentB.GetLetterGrade()}");
     }
 }
