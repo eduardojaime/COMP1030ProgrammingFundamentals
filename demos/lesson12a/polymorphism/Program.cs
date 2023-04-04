80c6e0e7a29809c9fb34c9d76409cdaf382a5547
Eduardo Jaime <eduardojaime86@gmail.com>
2023-04-04 19:21:22 -0400

    Implementing simple inheritance

--- demos/lesson12a/polymorphism/Program.cs
+++ demos/lesson12a/polymorphism/Program.cs
@@ -4,5 +4,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        // Declare and instantiate 3 objects
+        Cat myCat = new Cat();
+        Dog myDog = new Dog();
+        Goldfish dory = new Goldfish();
+        // Simple inheritance
+        dory.MakeSound();
+        
+        myCat.MakeSound();
+        myDog.MakeSound();
+
     }
 }
