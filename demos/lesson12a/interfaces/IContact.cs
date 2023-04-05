a132e976d0d9a1140075f7e57c4d2542a7b1ae49
Eduardo Jaime <eduardojaime86@gmail.com>
2023-04-04 20:27:22 -0400

    l12 interface definition

---
+++ demos/lesson12a/interfaces/IContact.cs
@@ -0,0 +1,6 @@
+// naming convention is to start interface names with 'I'
+interface IContact
+{
+    // best practice > keep interfaces short
+    string ShowContactInfo(); // If a class is a contact, it must show their info
+}
---
+++ demos/lesson12a/interfaces/IEmailable.cs
@@ -0,0 +1,6 @@
+interface IEmailable
+{
+    // Interfaces also represent 'attributes'
+    // In my system, a contact can be 'emailable'
+    bool SendEmail(); // an emailable contact in my system can be sent emails
+}
