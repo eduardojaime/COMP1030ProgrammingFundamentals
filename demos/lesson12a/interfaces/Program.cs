10f68203451a48427a8db150ce485a263a69a262
Eduardo Jaime <eduardojaime86@gmail.com>
2023-04-04 20:42:30 -0400

    l12 programming against the interface

--- demos/lesson12a/interfaces/Program.cs
+++ demos/lesson12a/interfaces/Program.cs
@@ -7,9 +7,25 @@ class Program
         Person myPerson = new Person();
         myPerson.FirstName = "Edward";
         myPerson.LastName = "James";
-        myPerson.PhoneNumber ="(647) 1234455";
+        myPerson.PhoneNumber = "(647) 1234455";
         myPerson.EmailAddress = "ed@georgian.ca";
-        Console.WriteLine(myPerson.SendEmail());
-        Console.WriteLine(myPerson.ShowContactInfo());        
+        //Console.WriteLine(myPerson.SendEmail());
+        Console.WriteLine(myPerson.ShowContactInfo());
+        SendExternalEmail(myPerson);
+        // new requirement, we need to handle Companies in the system     
+        Company myCompany = new Company();
+        myCompany.CompanyName = "Programmers Inc.";
+        myCompany.DirectorName = "James Edwards";
+        myCompany.DirectorEmailAddres = "jim@programmers.com";
+        myCompany.EmailAddress = "contact@programmes.com";
+        //Console.WriteLine(myCompany.SendEmail());
+        Console.WriteLine(myCompany.ShowContactInfo());
+        SendExternalEmail(myCompany);
+    }
+    // Another benefit is code reusability
+    // you can write methods that utilize interface as parameters
+    public static void SendExternalEmail(IEmailable contact)// wrong way >>> (Person person, Company company) {
+    {
+        Console.WriteLine(contact.SendEmail());
     }
 }
