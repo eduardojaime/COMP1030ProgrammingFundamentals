90146b1713b522f071461af0eee853b03496d42b
Eduardo Jaime <eduardojaime86@gmail.com>
2023-04-05 19:29:08 -0400

    l12 utilizing polymorphism in program.cs

--- demos/lesson12b/polymorphism/Program.cs
+++ demos/lesson12b/polymorphism/Program.cs
@@ -4,5 +4,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, Polymorphic Method!");
+        // simple inheritance Animal > Dog and Cat
+        Dog myDog = new Dog();
+        myDog.MakeSound();
+
+        Cat myCat = new Cat();
+        myCat.MakeSound();
     }
 }
