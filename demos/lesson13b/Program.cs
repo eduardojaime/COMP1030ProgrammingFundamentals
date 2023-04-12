823705bb85ff9f53989d657619ed62733971e67b
Eduardo Jaime <eduardojaime86@gmail.com>
2023-04-12 18:49:15 -0400

    l13 code files

---
+++ demos/lesson13b/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace lesson13b;
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool keepRunning = true;
+        while (keepRunning)
+        {
+            try
+            {
+                // 1) try catch blocks would usually go closer to the code that handles user input
+                Console.WriteLine("Hello, World!");
+                int[] myArr = { 1, 2, 3 };
+                Console.WriteLine("Please enter an index number to show the value: ");
+                // The following lines can break
+                string? choice = Console.ReadLine(); // depending on user input, setting nullable type (?) makes program expect and handle nulls
+                int index = int.Parse(choice); // can only parse any numeric (int) value represented as string
+                Console.WriteLine($"Index {index.ToString()} value is {myArr[index].ToString()}"); // if index is out of bounds
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Invalid numeric value. Please enter only digits.");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Invalid number. Please enter a number from 0 to 2.");
+            }
+            catch (Exception ex)
+            {
+                // Exception type catches everything
+                // Message is not very useful as it makes the user guess what they need to do to fix the issue
+                Console.WriteLine("Oops! Something happened: " + ex.Message); // format and index
+            }
+            Console.WriteLine("Exit?");
+            string exit = Console.ReadLine();
+            if (exit == "Y") { keepRunning = false; }
+        }
+    }
+}
